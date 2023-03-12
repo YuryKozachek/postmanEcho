ca42099e4ba6258d72c0e48239d9d9a670361690
Kozachek Yury <show_@mail.ru>
2023-03-12 10:14:18 +0300

    add fail test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -22,6 +22,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some dataA"));
     }
 }
